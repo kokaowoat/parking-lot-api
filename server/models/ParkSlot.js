679f29ff386c10292b00019438867037c5b6d8e1
Eknarin Thirayothin <oat@cashnow.co.th>
2020-12-11 15:11:51 +0700

    [Create] create-park-lost list api

--- server/models/ParkSlot.js
+++ server/models/ParkSlot.js
@@ -3,6 +3,35 @@
 const disable = require('../../common/helper').disableHelper;
 const rq = require('../../common/helper').httpHelper;
 
-module.exports = function(Parkslot) {
-  disable.disableAllMethods(Parkslot, ['create', 'find']);
+module.exports = function (ParkSlot) {
+  disable.disableAllMethods(ParkSlot, ['find']);
+
+  ParkSlot.createParkSlotList = async (carsize, amount, cb) => {
+    let createdCarSize = carsize ? carsize.toUpperCase() : null;
+    let createdSlot = [];
+    if (!createdCarSize || !amount || createdCarSize.replace(/\s/g, '') === '') {
+      return;
+    }
+    for (let i = 0; i < amount; i++) {
+      let res = await ParkSlot.create(
+        {
+          "carSize": createdCarSize
+        }
+      );
+      createdSlot.push(res);
+    }
+    return createdSlot;
+  }
+
+  ParkSlot.remoteMethod('createParkSlotList', {
+    http: {
+      path: '/slot-list',
+      verb: 'post',
+    },
+    accepts: [
+      { arg: 'carsize', type: 'string', 'required': true },
+      { arg: 'amount', type: 'number', 'required': true },
+    ],
+    returns: { arg: 'data', type: 'object', root: true },
+  });
 };
