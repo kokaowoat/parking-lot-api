5822f4fc29d5f26725684abc6ece6309c806d5c7
Eknarin Thirayothin <oat@cashnow.co.th>
2020-12-12 17:58:40 +0700

    [Update] unit test for car model

--- test/car.test.js
+++ test/car.test.js
@@ -1,23 +1,40 @@
-const {...helper} = require('./../common/helper');
+const rq = require('../common/helper').httpHelper;
 
-describe('Example for describe test suit', () => {
+describe('Car unit test', () => {
   beforeEach(() => {
   });
 
   afterEach(() => {
   });
 
-  test('access root path', async () => {
-    console.log('=== app ===');
-
+  test('Get all cars', async () => {
     try {
-      const response = await helper.httpHelper.request({
+      const response = await rq.request({
         method: 'GET',
-        uri: 'http://localhost:3000',
+        uri: 'http://localhost:3000/api/car',
       });
       expect(response.statusCode).toEqual(200);
     } catch (e) {
-      console.log('error', e);
+      throw new Error(e);
+    }
+  });
+
+  test('Get cars by car size M', async () => {
+    try {
+      const response = await rq.request({
+        method: 'GET',
+        uri: 'http://localhost:3000/api/car/plate-number?carSize=M',
+      });
+      const data = await JSON.parse(response.body);
+      let isTrueSize = true;
+      data.map(car => {
+        if (car.size !== 'M') {
+          isTrueSize = false;
+          return;
+        }
+      });
+      expect(isTrueSize).toEqual(true);
+    } catch (e) {
       throw new Error(e);
     }
   });
