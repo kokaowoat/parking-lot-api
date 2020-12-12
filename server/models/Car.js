3a49167793aa1ddaff84d4c1d954eb25363b3425
Eknarin Thirayothin <oat@cashnow.co.th>
2020-12-12 11:11:10 +0700

    [Create] api get car plateNumber list

--- server/models/Car.js
+++ server/models/Car.js
@@ -3,6 +3,31 @@
 const disable = require('../../common/helper').disableHelper;
 const rq = require('../../common/helper').httpHelper;
 
-module.exports = function(Car) {
+module.exports = function (Car) {
   disable.disableAllMethods(Car, ['create', 'find']);
+
+  Car.getPlaterNumberList = async (carSize, cb) => {
+    const cars = await Car.find(
+      {
+        where: { size: carSize? carSize.toUpperCase(): undefined },
+        fields: {
+          plateNumber: true,
+          size: true
+        },
+        order: ['size ASC', 'plateNumber ASC'],
+      }
+    );
+    return cars;
+  }
+
+  Car.remoteMethod('getPlaterNumberList', {
+    http: {
+      path: '/plate-number',
+      verb: 'get',
+    },
+    accepts: [
+      { arg: 'carSize', type: 'string', 'required': false },
+    ],
+    returns: { arg: 'data', type: 'object', root: true },
+  });
 };
